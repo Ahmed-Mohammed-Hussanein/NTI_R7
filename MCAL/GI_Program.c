eca65bdaf4a394856576af830ea28e19505c5a33
Ahmed Mohammed Hussanein Ali <am1233815@gmail.com>
2024-03-09 14:57:43 +0200

    Description of Global Interrupt Driver

--- MCAL/GI_Program.c
+++ MCAL/GI_Program.c
@@ -6,16 +6,38 @@
  */ 
 
 
+
+/************************************************************************/
+/*                  Global Interrupt Enable Driver                      */
+/*					AVR - ATMEGA32										*/
+/*					How to use this driver:								*/
+/*						- Include GI_interface.h						*/
+/*						- Use APIs according to its description			*/
+/************************************************************************/
+
 #include "common.h"
 
 #include "GI_interface.h"
 #include "GI_Register.h"
 
+
+/****************************************************************************************/
+/*			API Name: GI_voidEnable														*/
+/*			Parameters:	Void															*/
+/*			Return: Void																*/
+/*			Description: This API is used to Enable the Global Interrupt Controller		*/
+/****************************************************************************************/
 void GI_voidEnable(void)
 {
 	SET_BIT(GI_REG, I_bit);
 }
 
+/****************************************************************************************/
+/*			API Name: GI_voidDisable													*/
+/*			Parameters:	Void															*/
+/*			Return: Void																*/
+/*			Description: This API is used to Disable the Global Interrupt Controller	*/
+/****************************************************************************************/
 void GI_voidDisable(void)
 {
 	CLEAR_BIT(GI_REG, I_bit);
